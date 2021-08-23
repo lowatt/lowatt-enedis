d75214870eaae833b41b1fd31c612174ecd2400b
Philippe Pepiot <phil@lowatt.fr>
2021-08-23 15:11:23 +0200

    Make github release publish on pypi
    Add setuptools_scm and drop fixed version from setup.cfg
    
    Add a "tox -e packaging" environment (not run by default) that create a
    dist tarball and wheel distribution.
    
    Then configure github actions to run "tox -e packaging" and publish
    resulting packages.
    
    * on test.pypi.org for master branch. They are development release testing the pipeline is working fine.
    * on pypi.org for tagged releases
    
    This use secrets testpypi_password and pypi_password that are defined at
    the "lowatt organization" level. They are pypi tokens that can be
    revoked if needed.
    
    To make development release able to be uploaded we need to generate
    pep440 compatible versions, so add some code to remove the "+" part of
    versions generated by setuptools_scm.
    
    So to make a new relase we will just have to hit
    https://github.com/lowatt/lowatt-enedis/releases/new and this will
    create a tag and trigger github action and will finally publish on pypi.

--- setup.py
+++ setup.py
@@ -7,5 +7,18 @@
 
 from setuptools import setup
 
+
+def local_scheme(version: str) -> str:
+    """Generate a PEP440 compatible version if PEP440_VERSION is enabled"""
+    import os
+    import setuptools_scm.version  # only present during setup time
+
+    if "PEP440_VERSION" in os.environ:
+        return ""
+    version = setuptools_scm.version.get_local_node_and_date(version)
+    assert isinstance(version, str), version
+    return version
+
+
 if __name__ == "__main__":
-    setup()
+    setup(use_scm_version={"local_scheme": local_scheme})
